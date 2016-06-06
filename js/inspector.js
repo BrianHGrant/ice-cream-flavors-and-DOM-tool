02339f6f28f76667e34ed3e5ff5b0a7e55fbeee4
Brian Grant and Wolfgang Warneke <brian@>
2016-06-06 13:00:40 -0700

    add background color to inspector too, bug exists where color is permanent rather than temporary

--- js/inspector.js
+++ js/inspector.js
@@ -1,11 +1,17 @@
 var DOMelements = [];
 var message;
+var elementGlobal;
+var originalValue;
 $(document).ready(function() {
 
   $('*').click(function () {
+    if ( elementGlobal === DOMelements[0]) {
+            $("*").removeClass('pink');
+    }
     message = "";
     DOMelements.push($(this).prop('nodeName'));
     DOMelements.forEach(function(element) {
+      elementGlobal = element;
       if (element === DOMelements[0]) {
         message = "This element is a " + element + " element";
       } else if (element === DOMelements[DOMelements.length-1]) {
@@ -15,11 +21,16 @@ $(document).ready(function() {
       } else {
         message += " inside a " + element + " element";
       }
+
       if (message.includes('HTML')) {
         alert(message);
         DOMelements = [];
       }
     });
+    if ( elementGlobal === DOMelements[0]) {
+      $(this).addClass('pink');
+      $('.pink').css('background-color', 'pink');
+    }
 
   });
 });
