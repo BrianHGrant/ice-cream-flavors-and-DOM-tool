9f3ec9ed314ae6dc25f75f11af51b542ed902b0d
Brian Grant and Wolfgang Warneke <wolfgang@>
2016-06-06 10:32:56 -0700

    add initial filetree and array of favorite flavors

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  var flavors = ['chocolate','blue','Mountain Dew','egg','vanilla','fish and chips','salt','crushed up Goldfish'];
+  
+  // $("form input[name='submit']").click(function(event) {
+  //   event.preventDefault();
+  });
+});
